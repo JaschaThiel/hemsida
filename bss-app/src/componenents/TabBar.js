d4b5e8a0e26495900e3c1408b41059e5d9a6fe04
JaschaThiel <jascha.thiel@gmail.com>
2019-05-08 18:37:38 +0200

    minor clean up

--- bss-app/src/componenents/TabBar.js
+++ bss-app/src/componenents/TabBar.js
@@ -7,7 +7,6 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
 import ReactFitText from 'react-fittext';
 import Fab from '@material-ui/core/Fab';
 
