6451e5d53e11583235677f8b1ceb207acf13dfd0
JaschaThiel <jascha.thiel@gmail.com>
2020-03-26 22:49:00 +0100

    replaced ENG placeholder text with coming soon message

--- bss-app/src/componenents/ContactTextContainer.js
+++ bss-app/src/componenents/ContactTextContainer.js
@@ -90,17 +90,7 @@ function Types(props) {
     :
     <div className={classes.root}>
       <Typography variant="inherit" paragraph={true} >
-        Blabla about contact info Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-        unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-      </Typography>
-        <img className={classes.img1} src={ image1} alt="im1" />
-      <Typography variant="inherit" gutterBottom={true} >
-      body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-        unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-        unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+        Enlgish site coming soon, stay tuned!
       </Typography>
     </div>
   );
--- bss-app/src/componenents/ServicesTextExpansionContainer.js
+++ bss-app/src/componenents/ServicesTextExpansionContainer.js
@@ -211,16 +211,7 @@ class ServicesTextContainer extends React.Component {
     :
     <div className={classes.root}>
       <Typography variant="inherit" paragraph={true} >
-        Blabla info anbout possible services Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-        unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-      </Typography>
-      <Typography variant="inherit" gutterBottom={true} >
-      body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-        unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-        unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+        Enlgish site coming soon, stay tuned!
       </Typography>
     </div>
   );
