ae273c825e3187a3f015949c7f2de341c8f5d118
JaschaThiel <jascha.thiel@gmail.com>
2020-03-27 14:32:26 +0100

    addded de contact page content, and draft for eng temp version

--- bss-app/src/componenents/ContactTextContainer.js
+++ bss-app/src/componenents/ContactTextContainer.js
@@ -67,31 +67,86 @@ function Types(props) {
       <Typography variant="inherit" gutterBottom={true} align="left" >
         Genom många trädgårds-, renoverings- och byggprojekt på mina och andras fastigheter,
         och genom att ha jobbat mycket med kompisar med olika hantverksyrken har jag skaffat mig omfattande kunskaper som jag nu kan erbjuda dig.
-      <br></br><br></br>
+      </Typography>
+      <Typography variant="inherit" paragraph={true} align="left" >
         Mina intresseområden är ganska breda. Hantverk av alla slag och trädgård, odling, matlagning, natur och hållbarhet,
         både utifrån ett miljö- och ett samhällsperspektiv (om man nu kan skilja på dem). Jag gillar att lösa problem och jag tycker om ny kunskap.
       </Typography>
     </div>
     : language === 'de' ?
     <div className={classes.root}>
-      <Typography variant="inherit" paragraph={true} >
-        Blabla info zu kontakt undsoweiter bla Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-        unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+      <Typography variant="inherit" paragraph={true} align="left" >
+        Hier finden Sie Kontaktinformationen und weiter unten erzähle ich kurz über mich.
       </Typography>
-        <img className={classes.img1} src={ image1} alt="im1" />
-      <Typography variant="inherit" gutterBottom={true} >
-      body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-        unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-        unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+      <Typography variant="inherit" paragraph={true} align="left" >
+        Telefonisch erreichen Sie mich an Werktagen von 9:00 - 16:00 Uhr.
+        Es kann aber sein, dass ich mit etwas beschäftigt bin was mich hindert zu antworten oder was dazu führt,
+        dass ich das Telefon nicht höre. Hinterlassen Sie gerne eine Nachricht auf dem Anrufbeantworter oder,
+        besser noch, schicken Sie mir eine Mail, SMS, oder Nachricht über eine der unten genannten Sozialen Medien.
+        Ich melde mich dann so schnell wie möglich bei Ihnen.
+      </Typography>
+      <Typography variant="inherit" paragraph={true} align="left" >
+        <b>Gunnar Thiel</b>
+        <br></br>
+        Festnetz: 012 345 67 89<br></br>
+        Handy: 987 654 32 10<br></br><br></br>
+        E-mail: gunnar@blekingestugservice.se<br></br><br></br>
+        Soziale Medien: WhatsApp, Signal, Threema<br></br><br></br>
+        <u>Telefonzeiten:</u><br></br>
+        Werktags 9:00 - 16:00 Uhr<br></br>
+        Passen Ihnen die Zeiten nicht? Schicken Sie mir eine Nachricht mit einem Zeitvorschlag, ich rufe Sie dann an.<br></br><br></br>
+        <u>Anschrift:</u><br></br>
+        Sävsjömålavägen 34<br></br>
+        37262 Bräkne-Hoby<br></br><br></br>
+        Die Firma ist beim schwedischen Finanzamt registriert.
+        <br></br><br></br>
+      </Typography>
+      
+      <Typography color="secondary" component="h2" variant="h3" gutterBottom={true} align="left" >
+        Über mich
+      </Typography>
+      <Typography variant="inherit" paragraph={true} align="left" >
+        Geboren 1966 in Lübeck, aufgewachsen in Hamburg und San Diego. Verheiratet, zwei erwachsene und ausgeflogene Kinder.
+        Ich habe eine Ausbildung im Bereich der Sozialen Arbeit und habe einige Jahre in diesem Beruf gearbeitet.
+      </Typography>
+      <Typography variant="inherit" paragraph={true} align="left" >
+        Ich habe aber auch mehrere Jahre als Möbeltischler gearbeitet sowie viele Jahre als Hausmeister,
+        Lagerarbeiter und Fahrer für einen Schiffsausrüster im Hamburger Hafen. Ich habe sogar eine zeitlang als
+        Kundendienstmitarbeiter für eine Computerfirma gearbeitet, obwohl ich keine Ahnung habe von Computern. :-)
+      </Typography>
+      <img className={classes.img1} src={ image1} alt="im1" />
+      <Typography variant="inherit" paragraph={true} align="left" >
+        Durch viele Garten-, Renovierungs- und Bauprojekte auf und an meinen eigenen und anderen Grundstücken und
+        Häusern sowie durch viel Zusammenarbeit mit Freunden mit unterschiedlichen Handwerksausbildungen habe ich mir das
+        umfangreiche Wissen verschafft, dass ich Ihnen heute anbieten kann.
+      </Typography>
+      <Typography variant="inherit" gutterBottom={true} align="left" >
+        Meine Interessen sind recht vielfältig. Handwerk jeglicher Art, Garten, Gemüseanbau, Kochen, Natur und Nachhaltigkeit,
+        sowohl aus einer Umwelt-, wie auch aus einer Gesellschaftsperspektive (wenn man das denn trennen kann).
+        Ich habe Spaß daran, Probleme zu lösen und mir neues Wissen anzueignen.
       </Typography>
     </div>
     :
     <div className={classes.root}>
-      <Typography variant="inherit" paragraph={true} >
-        English site coming soon, stay tuned!
+      <Typography variant="inherit" paragraph={true} align="left" >
+        The English site is currently under construction. If you have any questions regarding services,
+        feel free to contact me. 
+      </Typography>
+      <Typography variant="inherit" paragraph={true} align="left" >
+        <b>Gunnar Thiel</b>
+        <br></br>
+        Telephone: 012 345 67 89<br></br>
+        Mobile: 987 654 32 10<br></br><br></br>
+        E-mail: gunnar@blekingestugservice.se<br></br><br></br>
+        Social media: WhatsApp, Signal, Threema<br></br><br></br>
+        <u>Phone hours:</u><br></br>
+        Weekdays kl. 09:00 - 16:00<br></br>
+        If the the phone hours do not work with you, or you have trouble getting a hold of me during them,
+        please send me a message and I will get back to you as soon as possible.
+        <br></br><br></br>
+        <u>Postadress:</u><br></br>
+        Sävsjömålavägen 34<br></br>
+        37262 Bräkne-Hoby<br></br><br></br>
       </Typography>
     </div>
   );
